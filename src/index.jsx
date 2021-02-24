1bcafe13740757e000cef3ac212cf0bb9f72dab4
Jonathan Rivest <jonathan_romero@hotmail.fr>
2021-02-24 12:41:56 +0000

    fichier JSON rempli

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import Appli from './Appli';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Appli />
   </React.StrictMode>,
   document.getElementById('racine')
 );
