1fdc5a3d422fb84b7a1670246b551cec72440f71
Jonathan Rivest <jonathan_romero@hotmail.fr>
2021-02-25 00:46:37 +0000

    finition

--- src/Signets.jsx
+++ src/Signets.jsx
@@ -1,15 +1,20 @@
 import './Signets.scss';
 import IconButton from '@material-ui/core/IconButton';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import SortIcon from '@material-ui/icons/Sort';
 
 
 export default function Signets(props) {
     return (
         <li className="Signets">
+          <div className = "sort"><SortIcon></SortIcon></div>
           <img src={'images/' + props.id + '.png'} alt={props.titre}/>
           <div className="leSignet">
             <p className="titre">{props.titre}</p>
             <p className="datemodif"><b>{props.datemodif}</b></p>
-            <p><IconButton></IconButton></p>
+            <p><IconButton>
+              <MoreVertIcon style= {{fill: "white"}}/>
+              </IconButton></p>
           </div>
           
           
